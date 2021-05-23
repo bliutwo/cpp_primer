f6ae8cac68d8b5c8c6c48adbcc1a9faf7609300e
bliutwo <bliutwo@cs.stanford.edu>
2021-05-23 14:53:34 -0700

    Rewrite exercises from 1.4.1 with for loops

--- inRange.cc
+++ inRange.cc
@@ -4,6 +4,7 @@ int main() {
     int m, n;
     std::cout << "Enter two integers: ";
     std::cin >> m >> n;
+    int origM = m;
     if (m > n) {
         std::cout << "Invalid range: " << m << " is greater than " << n << "." << std::endl;
         return 0;
@@ -11,6 +12,10 @@ int main() {
     while (m <= n) {
         std::cout << m << std::endl;
         m++;
+    }
+    m = origM;
+    for (int i = m; i <= n; i++) {
+        std::cout << i << std::endl;
     }
     return 0;
 }
--- while10to0.cc
+++ while10to0.cc
@@ -1,10 +1,14 @@
 #include <iostream>
 
 int main() {
-    int n = 10;
+    int orig = 10;
+    int n = orig;
     while (n >= 0) {
         std::cout << n << std::endl;
         n--;
+    }
+    for (int i = orig; i >= 0; i--) {
+        std::cout << i << std::endl;
     }
     return 0;
 }
