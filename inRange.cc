6e8ad2c6cea9920ef20f4a046158212de728110c
bliutwo <bliutwo@cs.stanford.edu>
2021-05-23 15:00:01 -0700

    Add handling for m > n

--- inRange.cc
+++ inRange.cc
@@ -4,16 +4,19 @@ int main() {
     int m, n;
     std::cout << "Enter two integers: ";
     std::cin >> m >> n;
-    int origM = m;
+    // int origM = m;
     if (m > n) {
-        std::cout << "Invalid range: " << m << " is greater than " << n << "." << std::endl;
+        // std::cout << "Invalid range: " << m << " is greater than " << n << "." << std::endl;
+        for (int i = m; i >= n; i--) {
+            std::cout << i << std::endl;
+        }
         return 0;
     }
-    while (m <= n) {
-        std::cout << m << std::endl;
-        m++;
-    }
-    m = origM;
+    // while (m <= n) {
+    //     std::cout << m << std::endl;
+    //     m++;
+    // }
+    // m = origM;
     for (int i = m; i <= n; i++) {
         std::cout << i << std::endl;
     }
